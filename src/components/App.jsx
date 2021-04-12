4ef4b6795844755b602b355867faa537924f0664
Rohit <48995037+rohit-vrrr@users.noreply.github.com>
2021-04-12 18:46:26 +0530

    Added source files
    + useState()

---
+++ src/components/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function App() {
+  const [count, setCount] = React.useState(0);
+
+  function increment() {
+    setCount(count + 1);
+  }
+
+  function decrement() {
+    setCount(count - 1);
+  }
+
+  return (
+    <div className="container">
+      <h1>{count}</h1>
+      <button onClick={increment}>+</button>
+      <button onClick={decrement}>-</button>
+    </div>
+  );
+}
+
+export default App;
